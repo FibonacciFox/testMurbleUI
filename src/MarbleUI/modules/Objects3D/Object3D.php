9079d65b2ed4dcf88078a43e0e3edece4ea19eef
ElGenKa <winrar.id10947798@gmail.com>
2021-04-03 23:30:42 +0300

    3d cube upd

--- src/MarbleUI/modules/Objects3D/Object3D.php
+++ src/MarbleUI/modules/Objects3D/Object3D.php
@@ -5,15 +5,24 @@ namespace MarbleUI\modules\Objects3D;
 use fibonaccifox\AppGameKit;
 use MarbleUI\modules\BaseObject;
 
+/**
+ * Class Text
+ *
+ * @property float $x - X position of a text object in world coordinates.
+ * @property float $y - Y position of a text object in world coordinates.
+ * @property float $z - Z position of a text object in world coordinates.
+ * @property array $position - Z position of a text object in world coordinates.
+ * @property bool $visible - Z position of a text object in world coordinates.
+ * @package MarbleUI\modules\Objects3D
+ */
 class Object3D extends BaseObject
 {
     /**
      * @var array
      */
-    private array $position;
-    private int $x;
-    private int $y;
-    private int $z;
+    private float $x;
+    private float $y;
+    private float $z;
     private bool $visible;
 
     var $Tag;
@@ -38,71 +47,95 @@ class Object3D extends BaseObject
         return $this->softDelete;
     }
 
-    public function __set($property, $value)
+    public function MoveX(float $x)
     {
-        //var_dump($property);
-        switch ($property) {
-            case 'position':
-                if (is_array($value)) {
-                    $this->position = $value;
-                    $this->agk->SetObjectPosition($this->objectId, $value[0], $value[1], $value[2]);
-                }
-                break;
-            case 'x':
-                $this->x = $value;
-                $this->y = $this->agk->GetObjectY($this->objectId);
-                $this->z = $this->agk->GetObjectZ($this->objectId);
-                $this->position = [$this->x, $this->y, $this->z];
-                $this->agk->SetObjectPosition($this->objectId, $this->x, $this->y, $this->z);
-                break;
-            case 'y':
-                $this->x = $this->agk->GetObjectX($this->objectId);
-                $this->y = $value;
-                $this->z = $this->agk->GetObjectZ($this->objectId);
-                $this->position = [$this->x, $this->y, $this->z];
-                $this->agk->SetObjectPosition($this->objectId, $this->x, $this->y, $this->z);
-                break;
-            case 'z':
-                $this->x = $this->agk->GetObjectX($this->objectId);
-                $this->y = $this->agk->GetObjectY($this->objectId);
-                $this->z = $value;
-                $this->position = [$this->x, $this->y, $this->z];
-                $this->agk->SetObjectPosition($this->objectId, $this->x, $this->y, $this->z);
-                break;
-            case 'visible':
-                $this->visible = $value;
-                $this->agk->SetObjectVisible($this->objectId, $value);
-                break;
-        }
-        parent::__set($property, $value);
+        $this->CheckPosition('x');
+        $this->x += $x;
+        $this->UpdatePosition();
     }
 
-    public function __get($property)
+    public function MoveY(float $y)
     {
-        switch ($property) {
-            case 'position':
-                $this->position = [ $this->agk->GetObjectX($this->objectId), $this->agk->GetObjectY($this->objectId), $this->agk->GetObjectZ($this->objectId) ];
-                return $this->position;
-                break;
-            case 'x':
-                $this->x = $this->agk->GetObjectX($this->objectId);
-                return $this->x;
-                break;
-            case 'y':
-                $this->y = $this->agk->GetObjectY($this->objectId);
-                return $this->y;
-                break;
-            case 'z':
-                $this->z = $this->agk->GetObjectZ($this->objectId);
-                return $this->z;
-                break;
-            case 'visible':
-                $this->visible = $this->agk->GetObjectVisible($this->objectId);
-                return $this->visible;
-                break;
-        }
-
-        return parent::__get($property);
+        $this->CheckPosition('y');
+        $this->y += $y;
+        $this->UpdatePosition();
     }
+
+    public function MoveZ(float $z)
+    {
+        $this->CheckPosition('z');
+        $this->z += $z;
+        $this->UpdatePosition();
+    }
+
+    public function SetX(float $x)
+    {
+        $this->CheckPosition('y');
+        $this->CheckPosition('z');
+        $this->x = $x;
+        $this->UpdatePosition();
+    }
+
+    public function SetY(float $y)
+    {
+        $this->CheckPosition('x');
+        $this->CheckPosition('z');
+        $this->y = $y;
+        $this->UpdatePosition();
+    }
+
+    public function SetZ(float $z)
+    {
+        $this->CheckPosition('x');
+        $this->CheckPosition('y');
+        $this->z = $z;
+        $this->UpdatePosition();
+    }
+
+    public function SetPosition(array $position)
+    {
+        $this->x = $position[0];
+        $this->y = $position[1];
+        $this->z = $position[2];
+        $this->UpdatePosition();
+    }
+
+    public function GetX()
+    {
+        return $this->x;
+    }
+
+    public function GetY()
+    {
+        return $this->y;
+    }
+
+    public function GetZ()
+    {
+        return $this->z;
+    }
+
+    public function GetPosition()
+    {
+        return [$this->x, $this->y, $this->z];
+    }
+
+    private function UpdatePosition()
+    {
+        $this->agk->SetObjectPosition($this->objectId, $this->x, $this->y, $this->z);
+    }
+
+    private function CheckPosition($orientation = false)
+    {
+        if ($orientation == 'x' or $orientation == false)
+            if ($this->agk->GetObjectX($this->objectId) != $this->x) $this->x = $this->agk->GetObjectX($this->objectId);
+        if ($orientation == 'y' or $orientation == false)
+            if ($this->agk->GetObjectY($this->objectId) != $this->y) $this->y = $this->agk->GetObjectX($this->objectId);
+        if ($orientation == 'z' or $orientation == false)
+            if ($this->agk->GetObjectZ($this->objectId) != $this->z) $this->z = $this->agk->GetObjectX($this->objectId);
+    }
+
+
+
 
 }
--- src/index3D.php
+++ src/index3D.php
@@ -8,12 +8,13 @@ class index3D
 {
     public AppGameKit $AppGameKit;
     private Core3D $Core3D;
+    var $textures;
 
     public function __construct()
     {
         $this->AppGameKit = new AppGameKit($this);
         $agk = $this->AppGameKit;
-        $agk->Init(1024, 768, false);
+        $agk->Init(1920, 1080, false);
     }
 
     public function Begin()
@@ -21,36 +22,70 @@ class index3D
         var_dump("Begin!");
         $agk = $this->AppGameKit;
         $agk->SetWindowTitle('Hello World');
-        $agk->setvirtualresolution(1024, 768);
-
+        $agk->SetVirtualResolution(1920, 1080);
+        $agk->SetVSync(1);
         $this->Core3D = new Core3D($agk);
-        $box = $this->Core3D->CreateBox();
-        $box->position = [0,0,0];
-        var_dump($box->objectId);
-        $box->Tag = 'MyBox';
-        $box->SetData('Strafe', false);
-        //var_dump($box->x);
+
+        $dir_link = "media/textures/";
+        $dir_data = scandir(realpath($dir_link));
+        var_dump($dir_data);
+
+        $id = 0;
+        foreach ($dir_data as $file){
+            if($file != "." and $file != ".." and $file != "player.png"){
+                $path = $agk->getPath("textures/$file");
+                var_dump($path);
+                $id_l = $agk->LoadImage($path);
+                var_dump($path);
+                //var_dump($id_l);
+                $this->textures[] = $id_l;
+                $id++;
+            }
+        }
+        //var_dump(1);
+        //var_dump($this->textures);
+        for ($i=-600; $i<=600; $i++) {
+            $box = $this->Core3D->CreateBox();
+            $box->SetPosition([$i*2, 0, 0]);
+            $box->Tag = 'MyBox';
+            $box->SetData('Strafe', false);
+
+            $this->AppGameKit->SetObjectImage($box->objectId, $this->textures[rand(0, $id)], 0);
+        }
+
+        //$this->AppGameKit->SetScreen
+        //var_dump($box->GetData('Strafe'));
     }
 
     public function Loop()
     {
-        $agk = $this->AppGameKit;
+        if(false == true) {
+            $agk = $this->AppGameKit;
 
-        $box = $this->Core3D->GetObjectWidthTag('MyBox'); //Вернет первый объект с таким тегом
-        //var_dump($box);
-        if( $box->GetData('Strafe') ){
-            $box->x += 0.05;
-        }else{
-            $box->x -= 0.05;
+            $boxs = $this->Core3D->GetObjectsWidthTag('MyBox'); //Вернет первый объект с таким тегом
+            //var_dump($boxs);
+            /**
+             * @var \MarbleUI\modules\Objects3D\Object3D $box
+             */
+            foreach ($boxs as $box) {
+
+                if ($box->GetX() != 0) {
+                    if ($box->GetData('Strafe')) {
+                        $box->MoveX(0.25);
+                    } else {
+                        $box->MoveX(-0.25);
+                    }
+                }
+                //var_dump($box->x);
+
+                //if ($box->GetX() < -6) $box->SetData('Strafe', true);
+                //if ($box->GetX() > 6) $box->SetData('Strafe', false);
+            }
+            //$b = $agk->MakeColor(0, 136, 255);
+            //$agk->DrawBox(500, 50, 650, 150, $b, $b, $b, $b, 1);
+            $agk->Print($agk->ScreenFPS());
+            $agk->Sync();
         }
-        //var_dump($box->x);
-
-        if($box->x < -2) $box->SetData('Strafe', true);
-        if($box->x > 2) $box->SetData('Strafe', false);
-
-        //$b = $agk->MakeColor(0, 136, 255);
-        //$agk->DrawBox(500, 50, 650, 150, $b, $b, $b, $b, 1);
-        $agk->Sync();
     }
 
     public function End()
